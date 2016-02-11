6b9592a93d4169eb008dbf80157b991bfa93fee7
lesomnus <somnus950301@gmail.com>
2016-02-12 03:03:12 +0900

    Add comment

--- src/app/interface.js
+++ src/app/interface.js
@@ -1,6 +1,7 @@
 (function () {
     console.log("!");
     
+    // Prevent conflict between jQuery and BSjs
     $.widget.bridge('uitooltip', $.ui.tooltip);
     $.widget.bridge('uibutton', $.ui.button);
 })();
