181a8deff96b95ac630ee7e85af3f4aa53e939fc
Kyungman <gracefulife@naver.com>
2016-03-01 02:16:15 +0900

    delete console log

--- src/modules/parser/comment.js
+++ src/modules/parser/comment.js
@@ -5,10 +5,8 @@
 define(function () {
     function comment(xml) {
         var rst = new Array;
-        console.log("in comment");
         var lists = xml.getElementsByClassName('reply_line');
         if (!lists.length) return rst;
-        console.log(lists)
         for (var i = 0; i < lists.length; ++i) {
             var itemBody = lists[i].children;
             var rstItem = new Object;
